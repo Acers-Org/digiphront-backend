f1ad24688ef6a57c0a1103c419af67f50d616aeb
Evelyn Anyebe <anyebeevelyn@gmail.com>
2022-04-12 17:10:13 +0100

    Start   work on courses

--- src/models/Course.js
+++ src/models/Course.js
@@ -1,26 +1,30 @@
 import mongoose from "mongoose";
 
-const CourseSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "must provide name"],
-    trim: true,
-    minlength: [5, "Name must be 5 or more characters"],
+const CourseSchema = new mongoose.Schema(
+  {
+    course_name: {
+      type: String,
+      required: [true, "must provide name"],
+      trim: true,
+      minlength: [5, "Name must be 5 or more characters"],
+    },
+    course_code: {
+      type: String,
+      required: [true, "must provide course code"],
+      trim: true,
+    },
+    description: String,
+    assessments: [],
+    department: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Department",
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
   },
-  description: String,
-  assessments: [],
-  duration: {
-    startDate: Date,
-    endDate: Date,
-  },
-  department: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "Department",
-  },
-  available: {
-    type: Boolean,
-    default: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Course", CourseSchema);
